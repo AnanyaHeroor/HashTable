f8d9c4e9f21479ba61a19a5843d2e10b2e4bd07a
Ananya Heroor <47873321+AnanyaHeroor@users.noreply.github.com>
2020-11-17 19:48:58 -0800

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -1,72 +1,115 @@
 // --== CS400 File Header Information ==--
-// Name: Chenyu Niu
-// Email: cniu8@wisc.edu
+// Name: Ananya Heroor
+// Email: heroor@wisc.edu
 // Team: LE
-// Role: Data Wrangler2
+// Role: Data Wrangler 1
 // TA: Divyanshu Saxena
 // Lecturer: Gary Dahl
-// Notes to Grader:none
-
+// Notes to Grader: My files are: CreditCard.java, CardCollection.java
+/**
+ * This class reads and saves the credit card data
+ * @author ananyaheroor
+ *
+ */
 public class CreditCard {
-    private Long cardNumber;
-    private String cardHolder;
-    private String expDate;
+  private Long cardNumber;
+  private String expDate;
+  private String cardHolder;
+  
+  public CreditCard(Long cardNumber, String expDate, String cardHolder) {
+    this.cardNumber = cardNumber;
+    this.expDate = expDate;
+    this.cardHolder = cardHolder;
+  }
+  
+  /**
+   * This method gets credit card numbers
+   * @return cardNumber
+   */
+  public Long getCardNumber() {
+    return cardNumber;
+  }
+  
+  /**
+   * This method sets credit card numbers
+   * @param cardNumber
+   * @return cardNumber
+   */
+  public void setCardNumber(Long cardNumber) {
+    this.cardNumber = cardNumber;
+  }
 
-    public CreditCard(Long cardNumber,String expDate, String cardHolder) {
-        this.cardNumber = cardNumber;
-        this.expDate = expDate;
-        this.cardHolder = cardHolder;
-    }
+  /**
+   * This method gets the credit card expiration dates
+   * @return expDate
+   */
+  public String getExpDate() {
+    return expDate;
+  }
 
-    public Long getCardNumber() {
-        return cardNumber;
-    }
+  /**
+   * This method sets the credit card expiration dates
+   * @param expDate
+   * @return expDate
+   */
+  public void setExpDate(String expDate) {
+    this.expDate = expDate;
+  }
 
-    public void setCardNumber(Long cardNumber) {
-        this.cardNumber = cardNumber;
-    }
+  /**
+   * This method gets the name of the credit card holder
+   * @return cardHolder
+   */
+  public String getCardHolder() {
+    return cardHolder;
+  }
 
-    public String getExpDate() {
-        return expDate;
-    }
+  /**
+   * This method sets the name of the credit card holder
+   * @param cardHolder
+   * @return cardHolder
+   */
+  public void setCardHolder(String cardHolder) {
+    this.cardHolder = cardHolder;
+  }
 
-    public void setExpDate(String expDate) {
-        this.expDate = expDate;
-    }
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((cardHolder == null) ? 0 : cardHolder.hashCode());
+    result = prime * result + ((cardNumber == null) ? 0 : cardNumber.hashCode());
+    result = prime * result + ((expDate == null) ? 0 : expDate.hashCode());
+    return result;
+  }
 
-    public String getCardHolder() {
-        return cardHolder;
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    CreditCard other = (CreditCard) obj;
+    if (cardHolder == null) {
+      if (other.cardHolder != null)
+        return false;
+    } else if (!cardHolder.equals(other.cardHolder))
+      return false;
+    if (cardNumber == null) {
+      if (other.cardNumber != null)
+        return false;
+    } else if (!cardNumber.equals(other.cardNumber))
+      return false;
+    if (expDate == null) {
+      if (other.expDate != null)
+        return false;
+    } else if (!expDate.equals(other.expDate))
+      return false;
+    return true;
+  }
 
-    public void setCardHolder(String cardHolder) {
-        this.cardHolder = cardHolder;
-    }
-
-//    @Override
-//    public int hashCode() {
-//        int hash = 5;
-//        hash = 89 * hash + (int) (this.cardNumber ^ (this.cardNumber >>> 32));
-//        return hash;
-//    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final CreditCard other = (CreditCard) obj;
-        if (this.cardNumber != other.cardNumber) {
-            return false;
-        }
-        return true;
-    }
-
+   
 
 }
-
